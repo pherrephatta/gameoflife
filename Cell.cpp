84b9f184411e191b259322858a67f87b7ddd197f
ArianeBeaudin <50454106+ArianeBeaudin@users.noreply.github.com>
2019-11-18 13:44:54 -0500

    cell.cpp

---
+++ Cell.cpp
@@ -0,0 +1,16 @@
+#include "Cell.h"
+
+State Cell::state() const
+{
+	return mState;
+}
+
+void Cell::setState(State state)
+{
+	mState = state;
+}
+
+void Cell::randomize(double probability)
+{
+	mState = (State)RandomUtil::generateEvent(probability);
+}
