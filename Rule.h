f4d2e0bcf5e68239971d121ea667d0a18abbb5c8
mcmelissa <49033878+mcmelissa@users.noreply.github.com>
2019-11-18 14:55:10 -0500

    rule.h

---
+++ Rule.h
@@ -0,0 +1,29 @@
+#ifndef RULE_H
+#define RULE_H
+
+#include <string>
+#include <vector>
+#include "Cell.h"
+
+
+class Rule {
+
+private:
+
+	//Separer les vivants des morts a partir des regles
+	std::vector<int> ruleAlive;
+	std::vector<int> ruleDead;
+
+	Rule() = default;
+	~Rule() = default;
+
+	//naviguer de regle en regle
+	void nextRule(int i);
+	void prevRule(int i);
+
+	//
+	bool stateOutcome(int nbAlive, Cell state);
+
+};
+
+#endif //RULE_H
