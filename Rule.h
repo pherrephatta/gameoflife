6df7131444acb28d84d84a3fa541b8f4916a2a08
mcmelissa <49033878+mcmelissa@users.noreply.github.com>
2019-11-18 16:36:41 -0500

    corrections rule.h

--- Rule.h
+++ Rule.h
@@ -7,21 +7,24 @@
 
 
 class Rule {
-
-private:
-
-	//Separer les vivants des morts a partir des regles
-	std::vector<int> ruleAlive;
-	std::vector<int> ruleDead;
-
+	
+public:
+	//constructeur et destructeur
 	Rule() = default;
 	~Rule() = default;
 
 	//naviguer de regle en regle
-	void nextRule(int i);
-	void prevRule(int i);
+	void nextRule();
+	void prevRule();
 
-	//
+	
+private:
+
+	//Separer les vivants des morts a partir des regles
+	std::vector<int> mRuleAlive;
+	std::vector<int> mRuleDead;
+
+	//permet de vérifier le prochain etat de la cellule selon son voisinage
 	bool stateOutcome(int nbAlive, Cell state);
 
 };
