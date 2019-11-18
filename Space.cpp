acc83f41e2a10fa5fc72cdc8de4ccc1107090c81
mcmelissa <49033878+mcmelissa@users.noreply.github.com>
2019-11-18 13:28:50 -0500

    le space

---
+++ Space.cpp
@@ -0,0 +1,23 @@
+#include "Space.h"
+#include "Cell.h"
+
+#include <algorithm>
+#include <sstream>
+
+
+Space::Space(int lenght, int height)
+	: mSpace(lenght*height), mLenght{ lenght }, mHeight{ height }
+{
+}
+
+
+void Space::setSpace() {
+	//RLE 
+}
+
+
+void Space::randomize(double probability) {
+	std::for_each(mSpace.begin(), mSpace.end(), [probability](Cell & cell)->void { cell.randomize(probability); });
+}
+
+
