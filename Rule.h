18e6a2a49f87b1a571cddeb46ca1f700dc946512
mcmelissa <49033878+mcmelissa@users.noreply.github.com>
2019-11-20 12:13:07 -0500

    updated

--- Rule.h
+++ Rule.h
@@ -5,24 +5,40 @@
 #include <vector>
 #include "Cell.h"
 
+using namespace std;
 
 class Rule {
-	
+
 public:
-	//constructeur et destructeur
-	Rule() = default;
-	~Rule() = default;
+
+	//fonction pour instancier le singleton
+	static Rule* getInstance();
 
 	//naviguer de regle en regle
 	void nextRule();
 	void prevRule();
 
-	
+	string getRule();
+
+	//regles (temporaire)
+	vector<string> mRules{ "B3/S23", "B36/S23", "B3678/S34678"};
+
+
 private:
 
+	//constructeur et destructeur
+	Rule() = default;
+	~Rule() = default;
+
+	//variable permettant l'instanciation
+	static Rule* mInstance;
+
+	//permet de naviguer entre les regles
+	vector<string>::iterator mIt = mRules.begin();
+
 	//Separer les vivants des morts a partir des regles
-	std::vector<int> mRuleAlive;
-	std::vector<int> mRuleDead;
+	vector<int> mRuleAlive;
+	vector<int> mRuleDead;
 
 	//permet de vérifier le prochain etat de la cellule selon son voisinage
 	bool stateOutcome(int nbAlive, Cell state);
