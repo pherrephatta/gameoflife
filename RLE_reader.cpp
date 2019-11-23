29439ae147fedce3593141b5636a9c06b0203763
pherrephatta <xenotimex@gmail.com>
2019-11-23 13:01:16 -0500

    merge RLE

---
+++ RLE_reader.cpp
@@ -0,0 +1,121 @@
+#include "RLE_reader.h"
+#include <iostream>
+#include <filesystem>
+
+
+
+RLE_reader::RLE_reader(string s) 
+	:mRLEfile{s}{
+	
+	if (!mRLEfile.is_open()) { cout << "Cannot open RLE file" << endl; }
+
+}
+
+bool RLE_reader::analyzeFile() {
+	smatch s;
+	string line;
+	bool inComments{ true };
+	bool endOfFile{ false };
+
+	while (mRLEfile.is_open()) {
+		while (mRLEfile.good() && inComments) {
+
+			getline(mRLEfile, line);
+			inComments = regex_match(line, s, mRgxComments);
+			if (!inComments) {
+				regex_match(line, s, mRgxInitLine);
+				//cout << s[0].str()<<endl;
+				if (s.length() > 0) {
+					mXWidth = stoi(s[1].str());
+					mYHeight = stoi(s[2].str());
+					if (mXWidth <1 || mYHeight <1) {
+						cout << "invalid size parameters"  << endl; //will likely never be reached b/c the regex makes sure there are no '-' in the line
+						return false;
+					}
+					regex_match(line, s, mRgxRuleFormat);//validation optionnelle de la regle
+					if (s.length() > 0) {
+						mRuleB = s[1].str();
+						mRuleS = s[2].str();
+					//we can "exit" the header with all necessary info
+					}
+					else{
+						cout << "invalid or inexistent rule format" << endl;
+					}
+				}
+				else {
+					cout << "Empty file, invalid comment, syntaxically invalid/inexistent initilization line, or invalid x y parameters" << endl;
+					cout << "Encountered error : " << line << endl;
+					//verification: we 1st check if we are in a valid comment(if we are, repeat). If not, we check for the init line regex. If it fails then its either an empty file, invalid commend, or bad init line. 
+					//verif presumes there are no useless newline chars.
+					return false;
+				}
+			}
+		}
+
+		while (mRLEfile.good() && !endOfFile) { //data part
+			getline(mRLEfile, line, '$');//takes following lines with $ as separator (bob$3o! = 2 lines)
+			line.erase(remove(line.begin(), line.end(), '\n'), line.end());
+			RLEbuffer b(line, mXWidth);
+			if (mExportUniverse.size()>(mXWidth*mYHeight)) {
+				cout << "Pattern overflow" << endl;
+				return false;
+			}
+			else if (b.analyzeDataString()) {
+			
+				vector<bool> freshUniverse = b.ExtractedUniverse();
+				mExportUniverse.insert(mExportUniverse.end(), freshUniverse.begin(), freshUniverse.end());
+				endOfFile = b.done();
+				//cout << line << "			freshU size:" << freshUniverse.size() << "			";
+				//cout << "Universe size after line : " << mExportUniverse.size() << endl << endl;
+			}
+			else {
+				cout << "incoherence in data (x overflow most likely)";
+				return false;
+			}
+
+		}
+
+		mRLEfile.close();
+	}
+
+	
+		for (int i{ 0 }; i < mXWidth*mYHeight; ++i) {
+			if (i%mXWidth == 0 && i>2) {
+				cout << '|'<<endl;
+			}
+			cout << (mExportUniverse.at(i)? '*' : ' ');
+		}
+		return true;
+}
+
+
+
+//C++ 17 NECESSARY
+std::vector<std::string> filesInPath(std::string folder) {
+    std::vector<std::string> files;
+    for (auto& file : std::filesystem::directory_iterator(folder)) {
+        if (file.is_regular_file()) {
+            std::filesystem::path path = file;
+            files.push_back(path.string());
+        }
+    }
+
+    return files;
+}
+
+
+
+void test(string s) {
+	RLE_reader t(s);
+	bool b = t.analyzeFile();
+	cout << "test : " << s << " result:  " << boolalpha << b << endl << endl;
+}
+
+int main() {
+
+	vector<string> testFiles = filesInPath("Y:\\C++ J_C\\en classe\\ECA_v4\\ex1v2\\ex1v2\\rle\\");
+	for_each(testFiles.begin(), testFiles.end(), [](string s)->void {cout << s << endl; });
+
+	for (string s : testFiles) 
+		test(s);
+	}
---
+++ RLEbuffer.cpp
@@ -0,0 +1,65 @@
+#include "RLEbuffer.h"
+#include <iostream>
+
+using namespace std;
+
+RLEbuffer::RLEbuffer(string s, size_t maxsize)
+	:mBufferSize{ 0 }, mExtractedUniverse{}, mMaxSize{ maxsize }, mCurrLine{s} {
+}
+
+bool RLEbuffer::verifyCoherence() {
+	return mExtractedUniverse.size() > mMaxSize ? false : true;
+}
+
+bool RLEbuffer::analyzeDataString() {
+	for (char c : mCurrLine) {
+
+		if (c == 'b' || c == 'o') {
+			mBufferSize = (mBufferSize == 0) ? 1 : mBufferSize; // si on a rien dans le buffer size, il est de 1 (avec l'état selon b ou o). pratique pour lire par ex: obo24b24o
+			for (int i{ 0 }; i < mBufferSize; ++i) {
+				mExtractedUniverse.push_back(c == 'o' ? true : false); // b = vivant, 0 = mort
+			}
+			if (!verifyCoherence()) { //si on fail le test d
+				return false;
+			}
+			mBufferSize = 0;
+		}
+		else if (isdigit(c)) {
+			mBufferSize *= 10; //on decale le chiffre actuel dans le buffer a gauche d'une coche (donc *10)
+			mBufferSize += c - '0'; //on ajoute a lextreme droite du buffer de chiffre le dernier. ex: 10, 10_, 104.
+			if (!verifyCoherence()) { return false; }
+		}
+		else if (c == '!' && mBufferSize == 0) { //si on croise un !, le tampon doit etre vide (ex: pas de bobo123!)
+			mDone = true;
+		}
+		else{
+			if (mDone&&(c == '#' || c== ' ' || c =='\n')) //si on a atteint la fin des données et que le prochain char est un debut de commentaire ou "rien"
+			{
+				return true;
+			}
+			cout << "Buffer: Irregulatity in char reading";
+			return false;
+		}
+
+	}
+	//si on se rend ici, a on évalué chaque char de la chaine
+	size_t valBeforeDollar = mBufferSize; //tldr: nb of dead lines to insert after current line. on garde en memoire la possible valoir numerique devant le signe de piasse; celui ci nous indique le nombre de lignes mortes a inserer apres avoir rempli le restant de la ligne actuelle avec des morts (si applicable).
+	
+	mBufferSize = mMaxSize - mExtractedUniverse.size();
+	if (mBufferSize != 0) { //s'il reste des espaces a remplir, on le remplit de mort
+		for (int i{ 0 }; i < mBufferSize; ++i) {
+			mExtractedUniverse.push_back(false); //
+		}
+	}
+
+	for (int i{ 0 }; i < (valBeforeDollar==0?0:valBeforeDollar-1); ++i) {//1(implicit) newline = 1 empty line,  2 retour de ligne = 1 ligne vide, 3 = 2 lignes vides
+		for (int j{ 0 }; j < mMaxSize; ++j) {
+			mExtractedUniverse.push_back(false); //pour chaque signe de piasse, on push back une ligne de x-longueur de morts
+		}
+	}
+
+	return true;
+}
+
+
+
