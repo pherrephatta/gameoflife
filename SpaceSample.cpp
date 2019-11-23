69316effe38fa8249c2c6d289f7e1b1595a650d4
ArianeBeaudin <50454106+ArianeBeaudin@users.noreply.github.com>
2019-11-23 12:54:35 -0500

    spacesample fonctionnel

--- SpaceSample.cpp
+++ SpaceSample.cpp
@@ -1,20 +1,13 @@
 #include "SpaceSample.h"
 #include <iostream>
 
-
-
-SpaceSample::SpaceSample(std::vector <Cell> const & space, std::vector<Cell>::const_iterator stateIt)
-	: mSpace{ space }, mStateIt{ stateIt }
+SpaceSample::SpaceSample(std::vector <Cell> const & space, std::vector<Cell>::const_iterator stateIt, int largeur)
+	: mSpace{ space }, mStateIt{ stateIt }, mLargeur{ largeur }
 {
-
-}
-
-
-
-void SpaceSample::setStateIt(std::vector<Cell>::const_iterator stateIt)
-{
-	mStateIt = stateIt;
-
+	mStateIt_haut = mStateIt;
+	mStateIt_bas = mStateIt;
+	
+	
 }
 
 std::vector<State> SpaceSample::Neighborhood()
@@ -24,6 +17,7 @@ std::vector<State> SpaceSample::Neighborhood()
 
 int SpaceSample::calculateNeighborhood()
 {
+	//TODO: Clear Vecteur ou Effacer ceux qu'on a plus besoin 
 	int nb_of_neighbors{};
 	for (int i{}; i < mNeighborhood.size(); ++i)
 	{
@@ -32,55 +26,83 @@ int SpaceSample::calculateNeighborhood()
 	return nb_of_neighbors;
 }
 
-void SpaceSample::GetNeighbors(std::vector<Cell>::const_iterator stateIt, int largeur)
-{
-	//mNeighborhood.resize(8);
-	setStateIt(stateIt);
-	mNeighborhood.push_back(right());
-	mNeighborhood.push_back(left());
-	mNeighborhood.push_back(down(largeur));
-	mNeighborhood.push_back(left());
-	mNeighborhood.push_back(right());
-	mNeighborhood.push_back(center());
-	mNeighborhood.push_back(up(largeur));
-	mNeighborhood.push_back(up(largeur));
-	mNeighborhood.push_back(left());
-	mNeighborhood.push_back(right());
-	mNeighborhood.push_back(center());
-
-
-
-	/*std::cout << (bool)mNeighborhood[0].state();
-	std::cout <<" ";
-	std::cout << (bool)mNeighborhood[1].state();*/
-
-}
-
-Cell const & SpaceSample::left() const
-{
-	return mStateIt == mSpace.begin() ? mSpace.back() : *std::prev(mStateIt);
-}
-
-Cell const & SpaceSample::center() const
-{
-	return *mStateIt;
-}
-
-Cell const & SpaceSample::up(int largeur)
-{
-	setStateIt(mStateIt - largeur);
-	return *mStateIt;
-}
-
-Cell const & SpaceSample::down(int largeur) 
+void SpaceSample::GetNeighbors(std::vector<Cell>::const_iterator it)
 {
 	
-
-	setStateIt(mStateIt+largeur);
-	return *mStateIt;
+	setIterateurs(it);
+	up();
+	middle(); 
+	down(); 
+	std::cout << "nb de voisins : " << calculateNeighborhood() << std::endl;
+	mNeighborhood.clear();
 }
 
-Cell const & SpaceSample::right() const
+Cell const & SpaceSample::center(std::vector<Cell>::const_iterator it) const
 {
-	return mStateIt == std::prev(mSpace.end()) ? mSpace.front() : *std::next(mStateIt);
+	return *it;
+}
+
+void SpaceSample::up()
+{
+
+	if ((int)(mStateIt - mSpace.begin()) < mLargeur)
+	{
+		mStateIt_haut = (mSpace.end() - mLargeur + (mStateIt - mSpace.begin()));
+	}
+	else mStateIt_haut =(mStateIt - mLargeur);
+	
+	mNeighborhood.push_back(left(mStateIt_haut));
+	mNeighborhood.push_back(center(mStateIt_haut));
+	mNeighborhood.push_back(right(mStateIt_haut));
+}
+
+void SpaceSample::middle()
+{
+
+	mNeighborhood.push_back(left(mStateIt));
+	mNeighborhood.push_back(right(mStateIt));
+}
+
+void SpaceSample::down() 
+{
+	if ((int)(mStateIt - mSpace.begin()) >= (mSpace.size()-mLargeur))
+		mStateIt_bas=(mSpace.begin() + ((mStateIt - mSpace.begin())% mLargeur));
+	else mStateIt_bas=(mStateIt + mLargeur);	
+
+	mNeighborhood.push_back(left(mStateIt_bas));
+	mNeighborhood.push_back(center(mStateIt_bas));
+	mNeighborhood.push_back(right(mStateIt_bas));
+}
+
+void SpaceSample::setIterateurs(std::vector<Cell>::const_iterator it)
+{
+	mStateIt = it;
+	mStateIt_bas = it;
+	mStateIt_haut = it;
+}
+
+
+Cell const & SpaceSample::right(std::vector<Cell>::const_iterator it) const
+{
+
+	if (it == mSpace.end() - 1)
+		return  *(it - mLargeur + 1);
+	else if (it - mSpace.begin() == (mLargeur - 1))
+		return  *(it +1 - mLargeur);
+	else if ((((it - mSpace.begin()) + 1) % (mLargeur)) == 0) {	
+		return *(it - mLargeur + 1);
+	}
+	else
+		return *std::next(it);
+}
+
+Cell const & SpaceSample::left(std::vector<Cell>::const_iterator it) const 
+{
+	if (it == mSpace.begin())
+	{
+		return  *(it + mLargeur - 1);
+	}else if ((it-mSpace.begin()) % mLargeur == 0)
+		return  *(it+(mLargeur-1));
+	else
+		return *std::prev(it);
 }
