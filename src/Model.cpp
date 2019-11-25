cc525ea83ed8a2a11c5f155e27fccbc6fac98e4b
soldari <0318122@decinfo.cvm>
2019-11-25 16:40:00 -0500

    model corrige

--- src/Model.cpp
+++ src/Model.cpp
@@ -1,31 +1,9 @@
 #include "Model.hpp"
 
 
-
-
-/*
-public:
-Model()		= default;
-~Model()	= default;
-
-//mettre a jour l'espace
-void updateSpace();
-
-//initialiser l'espace
-void initSpace();
-
-private:
-
-	vector<Rule> mRule;
-	Space* mSpace1;
-	Space* mspace2;
-	*/
-
 Model::Model(int width, int height) {
 	mSpace1 = new Space(width, height);
 	mSpace2 = new Space(*mSpace1);
-
-
 }
 
 Space& Model::space1() { return *mSpace1; }
@@ -39,24 +17,12 @@ void Model::updateSpace() {
 
 
 	for (; it != (*mSpace1).getSpace().end(); it++) {
-
-
-
-
-
 		(*it2).setState(((State)(regle1.outcome(((bool)(*it).state()), facade.GetNeighbors(it)))));
-
-		it2++;
-
-
+		++it2;
 	}
 
 	Space* temp{ mSpace1 };
 	mSpace1 = mSpace2;
 	mSpace2 = temp;
-
-
-
-
 
 }
